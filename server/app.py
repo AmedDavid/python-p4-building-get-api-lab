1baee8f111be9913fe09a89c6efa1bd1960355f0
David Amedi <amukoahdavid@gmail.com>
2025-07-06 23:09:37 +0300

    Implement bakery and baked goods endpoints to return JSON data

--- server/app.py
+++ server/app.py
@@ -20,19 +20,30 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_list = [bakery.to_dict(rules=("-baked_goods.bakery", "baked_goods")) for bakery in bakeries]
+    return make_response(jsonify(bakeries_list), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id)
+    bakery_dict = bakery.to_dict(rules=("-baked_goods.bakery", "baked_goods"))
+    return make_response(jsonify(bakery_dict), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [bg.to_dict(rules=("-bakery.baked_goods", "bakery")) for bg in baked_goods]
+    return make_response(jsonify(baked_goods_list), 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    bg = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if bg:
+        bg_dict = bg.to_dict(rules=("-bakery.baked_goods", "bakery"))
+        return make_response(jsonify(bg_dict), 200)
+    else:
+        return make_response(jsonify({}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
